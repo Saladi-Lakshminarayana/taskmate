47df17ef2728ed6aa847b05666082b7f83bc730b
Saladi-Lakshminarayana <saladilakshminarayana456@gmail.com>
2020-09-08 16:28:04 +0530

    Project updated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -1,4 +1,4 @@
-
+import django_heroku
 import os
 import environ
 
@@ -122,3 +122,4 @@ LOGIN_REDIRECT_URL = "todolist"
 LOGIN_URL = "login"
 
 # Heroku local settings
+django_heroku.settings(locals())
